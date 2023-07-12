dffaa6dda11912905a6318cbcbd9c98f13505d5e
KrishnaEzhumalayan <127396573+KrishnaEzhumalayan@users.noreply.github.com>
2023-07-12 12:18:19 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,9 +6,9 @@ $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "krishna45.ace@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = " . $name . "\r\n  Mobile =" . $mobile " \r\n  Email = " . $email . "\r\n Message = " . $message ;
 $headers = "From: krishna.ezhumalayan@gmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: syegaezhu@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
